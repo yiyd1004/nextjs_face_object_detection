ac2ed78130ebe71065ba57f600c5cf28f8faf5fe
Yong Yim <yiyd1004@gmail.com>
2024-03-12 22:56:57 -0700

    Update
    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: "/nextjs_face_object_detection",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
