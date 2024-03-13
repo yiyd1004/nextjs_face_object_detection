7f4d36e0079172489c2e47df759db3817e95ef91
Yong Yim <yiyd1004@gmail.com>
2024-03-12 23:15:29 -0700

    Update
    add base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: "/nextjs_face_object_detection",
     images: {
         unoptimized: true,
     },
