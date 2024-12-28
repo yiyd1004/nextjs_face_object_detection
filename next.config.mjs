7fa3ac6f41e7f7bd6677d34300dff696780a7d47
Yong Yim <yiyd1004@gmail.com>
2024-12-28 11:09:53 -0800

    Update
    Add basePath back into config file in order to deploy github page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    //Remove basePath when you are trying to deploy other than github page
+    basePath: "/nextjs_face_object_detection",
     images: {
         unoptimized: true,
     },
