6f7e47d324a807f0f56b70337576c9355d5b9170
Yong Yim <yiyd1004@gmail.com>
2024-12-28 10:29:41 -0800

    Update
    Removed basePath commented line
    Removed CORS header for test

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    //basePath: "/nextjs_face_object_detection",
     images: {
         unoptimized: true,
-    },
-    headers: async () => {
-        return [
-            {
-                source: "/",
-                headers: [
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET, POST, PUT, DELETE, OPTIONS",
-                    },
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value: "*",
-                    },
-                ],
-            },
-        ];
     },
 };
 
